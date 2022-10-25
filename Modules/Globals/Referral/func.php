e8bdbacad2c7be4c285202237f4f52c16320a002
TransTechService <proskura116@gmail.com>
2022-10-25 15:58:23 +0500

    Активация индивидуальной реф системы
    Активация индивидуальной реф системы

--- Modules/Globals/Referral/func.php
+++ Modules/Globals/Referral/func.php
@@ -14,17 +14,19 @@ class func
 {
 
     public $this_main = false;
-    public $shop = array();
-
     public function __construct($this_main)
     {
         /**@var $this_main \Modules\Globals\Referral\Referral*/
         $this->this_main = $this_main;
-        $this->shop = &get_instance()->shop;
+
     }
 
     public function widget_referral(){
         if (get_instance()->check_plugin('referral') AND (isset(get_instance()->config['referral']['status']) AND get_instance()->config['referral']['status'])){
+
+            if ((isset(get_instance()->config['referral']['individual']) AND get_instance()->config['referral']['individual']) AND get_instance()->session->session["user_data"]['referral']['individual'] != 1)
+                return '';
+
             return get_instance()->fenom->fetch(
                 get_tpl_file('widget_referral.tpl', get_class($this->this_main)),
                 get_lang('referral.lang')
